b763b8476850cfe9448af11755405bf8f8b8010f
shushu1234503 <2080258935@qq.com>
2026-04-06 22:34:19 +0800

    正在写关卡状态的脚本， 先提交一次

---
+++ The Old Man and the Insect/Assets/Scrips/第一部分/LevelStateManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//ok啊也是偷懒直接写一个伪状态机
+public enum LevelState
+{
+   OnEnterGame,
+   KnockingDoor,
+}
+public class LevelStateManager : MonoBehaviour
+{   
+    
+    public float Delay_Before_Knocking = 2f;
+    private LevelState currentState;
+    private LevelState lastState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = LevelState.OnEnterGame;
+        lastState = LevelState.OnEnterGame;
+       //待实现：播放音效，等写了音效管理系统
+       print("吱吱吱");
+       StartCoroutine(DelayToSwitchState(LevelState.KnockingDoor, Delay_Before_Knocking));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentState != lastState)//进入新状态执行一次
+        {
+            switch (currentState)
+            {   
+               case LevelState.KnockingDoor:
+                   print("咚咚咚");
+                   break;
+            }
+            lastState = currentState;
+        }
+        
+        
+        switch (currentState)
+        {
+            
+        }
+    }
+
+    // 每个状态的具体逻辑处理
+   
+    // 切换状态
+    private void SwitchState(LevelState newState)
+    {
+        currentState = newState;
+    }
+    
+    IEnumerator DelayToSwitchState(LevelState newState,float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        SwitchState(newState);
+    }
+}

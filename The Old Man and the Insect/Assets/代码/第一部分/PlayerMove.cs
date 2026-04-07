21c2be496169fa20385beaf18695f436f2430fa2
Frog-guagua <3028879265@qq.com>
2026-04-07 19:10:58 +0800

    正式添加了完全版状态机和走路脚本
    已通过实验

--- The Old Man and the Insect/Assets/代码/第一部分/PlayerMove.cs
+++ The Old Man and the Insect/Assets/代码/第一部分/PlayerMove.cs
@@ -2,25 +2,124 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 //呱：写在最前面 ——> 这个脚本挂在玩家身上
 public class PlayerMove : MonoBehaviour
 {
-    //[玩家固定步长] ： 这个美术老大自己来调数值 可以随便安个
-    [SerializeField]  float PlayerMoveLength_X =0;
-    [SerializeField]  float PlayerMoveLength_Y =0;
-    
-    //[玩家坐标]
+    #region 玩家
+
+    [SerializeField] float PlayerMoveLength_X = 0;
+    [SerializeField] float PlayerMoveLength_Y = 0;
     private Transform PlayerTransform;
+    public static bool IsMove = false;
+    private Animation PlayerAnimation;
+    private string currentAnimName = "";  
     
+    private Animator playerAnimator;
+
+    #endregion
+
+    enum E_PlayerState
+    {
+        Front,
+        Back,
+        TowardsLeft,
+        TowardsRight,
+    }
+    E_PlayerState playerState = E_PlayerState.TowardsLeft;
+    private Vector3 originalScale;
+    
+
     void Start()
     {
-        
+        #region 初始化玩家组件 数据
+        PlayerTransform = transform;
+        PlayerAnimation = PlayerTransform.GetComponent<Animation>();
+        originalScale = PlayerTransform.localScale;
+
+
+        playerAnimator = GetComponent<Animator>();
+        #endregion
     }
 
-    
+
     void Update()
+    { 
+        PosUpdate();
+        Move();
+    }
+
+    bool Move()
     {
+        float newX = PlayerTransform.position.x;
+        float newY = PlayerTransform.position.y;
+    
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newY = PlayerTransform.position.y + PlayerMoveLength_Y * Time.deltaTime;
+            IsMove = true;
+            playerState = E_PlayerState.Back;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newY = PlayerTransform.position.y - PlayerMoveLength_Y * Time.deltaTime;
+            IsMove = true;
+            playerState = E_PlayerState.Front;
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newX = PlayerTransform.position.x - PlayerMoveLength_X * Time.deltaTime;
+            IsMove = true;
+            playerState = E_PlayerState.TowardsLeft;
+            SetSideTowards(E_PlayerState.TowardsLeft);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newX = PlayerTransform.position.x + PlayerMoveLength_X * Time.deltaTime;
+            IsMove = true;
+            playerState = E_PlayerState.TowardsRight;
+            SetSideTowards(E_PlayerState.TowardsRight);
+        }
+        else
+        {
+            IsMove = false;
+        }
+
+        PlayerTransform.position = new Vector3(newX, newY, PlayerTransform.position.z);
+
+       
         
+            playerAnimator.SetBool("IsMoving", IsMove);
+    
+         
+            bool isSide = (playerState == E_PlayerState.TowardsLeft || playerState == E_PlayerState.TowardsRight);
+            playerAnimator.SetBool("IsSide", isSide);
+    
+           
+            float moveY = 0f;
+            if (playerState == E_PlayerState.Back)
+                moveY = 1f;
+            else if (playerState == E_PlayerState.Front)
+                moveY = -1f;
+       
+            playerAnimator.SetFloat("MoveY", moveY);
+
+        return IsMove;
+    }
+
+    void SetSideTowards(E_PlayerState targetState)
+    {
+        if (targetState == E_PlayerState.TowardsRight)
+        {
+            PlayerTransform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+        }
+        else if (targetState == E_PlayerState.TowardsLeft)
+        {
+            PlayerTransform.localScale = originalScale;
+        }
+    }
+    
+    void PosUpdate()
+    {
+        PlayerTransform = transform;
     }
 }

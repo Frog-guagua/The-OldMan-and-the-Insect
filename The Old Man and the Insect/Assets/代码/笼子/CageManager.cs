092e3fb4b741adac6c1184f73ad36ef7fcd0f3c8
shushu1234503 <2080258935@qq.com>
2026-04-08 22:54:47 +0800

    完善各种乱七八糟，写到点击桌子打开养成界面。

--- The Old Man and the Insect/Assets/代码/笼子/CageManager.cs
+++ The Old Man and the Insect/Assets/代码/笼子/CageManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,36 +14,30 @@ public struct InsectData//这个玩意作为正式使用的虫虫数据集
     public string description;
 }
 
-public class CageManager : MonoBehaviour
+public sealed class CageManager : MonoBehaviour
 {
     // 单例
 
     #region
 
+    private static readonly CageManager _instance = new CageManager();
+
     private CageManager()
     {
     }
 
-    // 静态字段保存单例实例
-    private static CageManager _instance;
-
-    // 静态属性提供对单例实例的访问
-    public static CageManager Instance
-    {
-        get
-        {
-            
-            return _instance;
-        }
-    }
+    public static CageManager Instance => _instance;
 
     #endregion
 
+    public GameObject canvas;
+
     public Dictionary<int, InsectData> insectInCage = new Dictionary<int, InsectData>();
+
     // 键和值分别为格子id和该格子内存的数据。
     //格子id为for循环生成每个格子时的那个i，具体见cageui
-    public List<Button> slotList = new List<Button>();//格子列表，方便对应格子id,刷新背包
-    public InsectData currentChosenData=new InsectData();//这个玩意用来存当前选中的虫虫数据，想必将来有大用
+    public List<Button> slotList = new List<Button>(); //格子列表，方便对应格子id,刷新背包
+    public InsectData currentChosenData = new InsectData(); //这个玩意用来存当前选中的虫虫数据，想必将来有大用
 
     public int slotCount = 20;
 
@@ -54,32 +47,26 @@ public class CageManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        else
-        {
-            _instance = this;
-        }
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
-
         for (int i = 0; i < slotCount; i++)
         {
             insectInCage.Add(i, new InsectData());
         }
     }
 
-// Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
     }
 
-// 增加昆虫
+    // 增加昆虫
     public void AddInsect(InsectData insectData)
     {
-        // 遍历所有格子，找到第一个空格子
         foreach (var kvp in insectInCage)
         {
             if (kvp.Value.insectId == 0) //  insectId 为 0 表示空格子
@@ -89,20 +76,18 @@ public class CageManager : MonoBehaviour
                 return;
             }
         }
-
-        
     }
-    public void AddInsect(InsectDataSO insectData)//这是一个重载，可以直接传so文件。
-    //后续如果方便的话可能会将所有等级数据全部写成so
+
+    public void AddInsect(InsectDataSO insectData) //这是一个重载，可以直接传so文件。
+        //后续如果方便的话可能会将所有等级数据全部写成so
     {
-        InsectData data=new InsectData();
+        InsectData data = new InsectData();
         data.insectId = insectData.insectId;
-        data.insectAtk=insectData.insectAtk;
-        data.insectHP=insectData.insectHP;
-        data.insectName=insectData.insectName;
-        data.description=insectData.description;
-        data.pointsComsumption=insectData.pointsComsumption;
-        // 遍历所有格子，找到第一个空格子
+        data.insectAtk = insectData.insectAtk;
+        data.insectHP = insectData.insectHP;
+        data.insectName = insectData.insectName;
+        data.description = insectData.description;
+        data.pointsComsumption = insectData.pointsComsumption;
         foreach (var kvp in insectInCage)
         {
             if (kvp.Value.insectId == 0) // 假设 insectId 为 0 表示空格子
@@ -112,11 +97,9 @@ public class CageManager : MonoBehaviour
                 return;
             }
         }
-       
-        
     }
 
-// 删除昆虫
+    // 删除昆虫
     public void RemoveInsect(int slotID)
     {
         if (insectInCage.ContainsKey(slotID))
@@ -124,38 +107,36 @@ public class CageManager : MonoBehaviour
             insectInCage[slotID] = new InsectData(); // 清空数据
             refreshSlot(slotID);
         }
-        
     }
 
-// 查询昆虫
+    // 查询昆虫
     public InsectData GetInsect(int slotID)
     {
         if (insectInCage.ContainsKey(slotID))
-        {   
+        {
             return insectInCage[slotID];
         }
         else
         {
-          
             return new InsectData();
         }
     }
 
-// 修改昆虫
+    // 修改昆虫
     public void UpdateInsect(int slotID, InsectData newInsectData)
     {
         if (insectInCage.ContainsKey(slotID))
-        {   
-            
+        {
             insectInCage[slotID] = newInsectData;
             refreshSlot(slotID);
         }
-      
     }
 
-    public void refreshSlot(int slotid)//更新格子ui
+    public void refreshSlot(int slotid) //更新格子ui
     {
         CageSlot cageSlot = slotList[slotid].GetComponent<CageSlot>();
         cageSlot.refreshSlot();
     }
+
+    
 }
